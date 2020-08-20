cb39082f2b093034e02118a6bad6e2c495ecef94
restufh12 <restufh12@gmail.com>
2020-08-21 00:12:05 +0700

    Edit Controller Home

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Home extends CI_Controller {
 	public function index()
 	{
-		$data = array('container' => 'detail-event');
+		$data = array('container' => 'home');
 		$this->load->view('templates\templates', $data);
 	}
 }
